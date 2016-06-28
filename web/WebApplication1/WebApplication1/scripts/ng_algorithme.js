22fdfc06af5c85cae2af0f4046430363389ce2e8
Ronpiche <vgspm@hotmail.fr>
2016-06-28 15:23:08 +0200

    merging

--- web/WebApplication1/WebApplication1/scripts/ng_algorithme.js
+++ web/WebApplication1/WebApplication1/scripts/ng_algorithme.js
@@ -75,8 +75,8 @@ algoApp.controller('mainController', ['$scope', 'constellationConsumer', 'conste
 
         $scope.listeAlgos = [];
 
-        constellation.intializeClient("http://localhost:8088", "8dea78b76b83d2ea291ed68db80e5cb1fd630ec8", "constellationAlgoApp");
-        controller.intializeClient("http://localhost:8088", "8dea78b76b83d2ea291ed68db80e5cb1fd630ec8", "controllerAlogApp");
+        constellation.intializeClient("http://localhost:8088", "1182b2384b6f311117b8fbbf4c37087982007885", "constellationAlgoApp");
+        controller.intializeClient("http://localhost:8088", "1182b2384b6f311117b8fbbf4c37087982007885", "controllerAlogApp");
 
         constellation.onUpdateStateObject(function (stateobject) {
             $scope.$apply(function () {
